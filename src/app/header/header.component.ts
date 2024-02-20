a07e8477d08d0612531b716343a7b989046f57c7
Benjamin <benjaminberthet18@gmail.com>
2024-02-20 15:20:58 +0100

    Changement de la couleur du background + Opti chargement image

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -4,11 +4,12 @@ import { HoverFocusDirective } from '../shared/directives/hover-focus-directive.
 import { ScrollListenerDirective } from '../shared/directives/scroll-listener-directive.directive';
 import { gsap } from "gsap";
 import { TextPlugin } from "gsap/TextPlugin";
+import { NgOptimizedImage } from '@angular/common'
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [CommonModule, HoverFocusDirective, ScrollListenerDirective],
+  imports: [CommonModule, HoverFocusDirective, ScrollListenerDirective, NgOptimizedImage],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css'
 })
