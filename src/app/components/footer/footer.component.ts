0015e21a05d42c03399616ebdc5bb58092e8958a
Benjamin <benjaminberthet18@gmail.com>
2024-02-21 18:24:45 +0100

    Ajout footer

--- src/app/components/footer/footer.component.ts
+++ src/app/components/footer/footer.component.ts
@@ -1,9 +1,13 @@
 import { Component } from '@angular/core';
+import { HoverFocusDirective } from '../../directives/hover-focus-directive.directive';
+import { NgOptimizedImage } from '@angular/common';
 
 @Component({
   selector: 'app-footer',
   standalone: true,
-  imports: [],
+  imports: [
+    HoverFocusDirective,
+    NgOptimizedImage],
   templateUrl: './footer.component.html',
   styleUrl: './footer.component.css'
 })
